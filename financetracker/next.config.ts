696992187ab4b1244af2f7618cbe21e352606c0b
Vathana <thava1206@gmail.com>
2026-04-25 13:30:55 -0500

    Backend maven added + configuration

--- financetracker/next.config.ts
+++ financetracker/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    allowedDevOrigins: ['169.254.150.213'],
 };
 
 export default nextConfig;
